9a277eee93b371defff2d5db7b45d2a1da606409
Letícia Maria Pequeno Madureira <leticiapequeno30@gmail.com>
2021-11-11 14:10:30 -0300

    First example; guess number game

---
+++ guessing_game/target/debug/deps/guessing_game-4bd3538acd61b1c5.d
@@ -0,0 +1,5 @@
+/home/leticia/guessing_game/target/debug/deps/guessing_game-4bd3538acd61b1c5: src/main.rs
+
+/home/leticia/guessing_game/target/debug/deps/guessing_game-4bd3538acd61b1c5.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/leticia/guessing_game/target/debug/guessing_game: /home/leticia/guessing_game/src/main.rs
