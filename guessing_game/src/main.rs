c92b41d6468851d44b1e3de282d90f90150c45b6
Leticia-maria <leticiapequeno30@gmail.com>
2021-12-08 14:12:12 -0300

    add random number generator

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,10 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
         println!("Guess the number!");
+        let secret_number = rand::thread_rng().gen_range(1..101);
+        println!("The secret number is: {}", secret_number);
         println!("Please input your guess.");
         let mut guess = String::new();
 
