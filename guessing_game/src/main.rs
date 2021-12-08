718cfcd82d378a31e8c30be0b341503dc0111e15
Leticia-maria <leticiapequeno30@gmail.com>
2021-12-08 14:16:09 -0300

    Compare guess number to random

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
         println!("Guess the number!");
@@ -12,4 +13,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
         println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!")
+            Ordering::Equal => println!("You win!")
+        }
 }
